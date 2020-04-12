5978e60cb9cb7a910dda00982edbd2c45cf2b4a9
Ruben Villota <rubenvillo04@gmail.com>
2020-04-12 13:46:41 -0500

    Fix navbar location inside component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
